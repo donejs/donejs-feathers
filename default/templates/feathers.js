4d63a7d4f110f7d3d041ef9972e03035eb01fc9e
Marshall Thompson <marshall@creativeideal.net>
2016-09-06 14:42:14 -0600

    Add comments to feathers config.
    Also included `allowSocketIO` option.

--- default/templates/feathers.js
+++ default/templates/feathers.js
@@ -1,11 +1,18 @@
 import Feathers from 'can-connect-feathers';
 
 const feathers = new Feathers({
+  // The URL of the Feathers API server.
   url: '<%= feathersUrl %>',
+  // The default `idProp` for all services.
   idProp: '<%= idProp %>',
+  // The key name of the location where the token will be stored.
   tokenLocation: 'feathers-jwt',
+  // The endpoint for token authentication.
   tokenEndpoint: 'auth/token',
-  localEndpoint: 'auth/local'
+  // The endpoint for username/password authentication.
+  localEndpoint: 'auth/local',
+  // Set to false to disable socketio and force any socketio services to switch to rest.
+  allowSocketIO: true
 });
 
 export default feathers;
