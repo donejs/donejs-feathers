7467bb7dad86a86fa186795cd54bb2cc47e099f2
Marshall Thompson <marshall@creativeideal.net>
2016-09-30 17:08:29 -0600

    properly bind subpage and session in page-auth

--- default/templates/page-auth/page-auth.js
+++ default/templates/page-auth/page-auth.js
@@ -4,11 +4,11 @@ import './page-auth.less!';
 import template from './page-auth.stache!';
 
 export const ViewModel = DefineMap.extend({<% if(enableSignup){ %>
-  authTab: {}, // bound to the subpage in the AppState.
+  subpage: {}, // bound to the subpage in the AppState.
 <% } %><% if(providers.includes('local')){ %>  session: '*',
 <% } %>  isOAuthRoute: {
     get(){
-      return this.authTab === 'success' || this.authTab === 'failure';
+      return this.subpage === 'success' || this.subpage === 'failure';
     }
   }
 });
